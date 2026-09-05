829f5b9e5a28ef9f9a9a68a685f36d7100da1df6
Akshit Rawat <Akshit.37428@stu.upes.ac.in>
2026-09-05 11:40:22 +0530

    Added program 53

---
+++ 53prog.c
@@ -0,0 +1,49 @@
+//Q53: Write a program to print the following pattern:
+/*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+
+*/
+#include <stdio.h>
+
+int main() {
+    int i, j, stars;
+
+    for (i = 1; i <= 9; i++) {
+
+        if (i <= 5) {
+            stars = 2 * i - 1;
+        } else {
+            stars = 2 * (9 - i) + 1;
+        }
+
+        for (j = 1; j <= stars; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
