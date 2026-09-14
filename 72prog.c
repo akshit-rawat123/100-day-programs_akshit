f9553c7039bdc29c6c9166e786742e08235e7299
Akshit Rawat <Akshit.37428@stu.upes.ac.in>
2026-09-14 12:18:27 +0530

    Added program 72

---
+++ 72prog.c
@@ -0,0 +1,43 @@
+//Q72: Find the sum of all elements in a matrix.
+
+/*
+Sample Test Cases:
+Input 1:
+2 3
+1 2 3
+4 5 6
+Output 1:
+21
+
+*/
+#include <stdio.h>
+
+int main() {
+    int matrix[10][10];
+    int rows, cols, i, j;
+    int sum = 0;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+
+    printf("Enter the elements of the matrix:\n");
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            sum = sum + matrix[i][j];
+        }
+    }
+
+    printf("Sum of all elements = %d", sum);
+
+    return 0;
+}
