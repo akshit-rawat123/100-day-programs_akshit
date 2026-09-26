d04d32f1a7416f7f4bf572b50d87eed117708cea
Akshit Rawat <Akshit.37428@stu.upes.ac.in>
2026-09-26 18:44:24 +0530

    Added program 91

---
+++ 91prog.c
@@ -0,0 +1,40 @@
+//Q91: Remove all vowels from a string.
+
+/*
+Sample Test Cases:
+Input 1:
+education
+Output 1:
+dctn
+
+*/
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, j = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[i] != '\0') {
+
+        if (str[i] != 'a' && str[i] != 'e' &&
+            str[i] != 'i' && str[i] != 'o' &&
+            str[i] != 'u' && str[i] != 'A' &&
+            str[i] != 'E' && str[i] != 'I' &&
+            str[i] != 'O' && str[i] != 'U') {
+
+            str[j] = str[i];
+            j++;
+        }
+
+        i++;
+    }
+
+    str[j] = '\0';
+
+    printf("String after removing vowels: %s", str);
+
+    return 0;
+}
